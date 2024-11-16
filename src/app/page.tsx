14c423622d5fbd4ca1a190d6c6ade3a9ac0c4cf6
JCEleanor <crcky4826@gmail.com>
2024-11-16 20:18:57 +0800

    fix: modify style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <div className="grid grid-cols-2 gap-8">
       <div
         // FIXME: position not sticky
-        className="my-20 flex flex-col justify-between sticky top-[8rem] h-[80vh]"
+        className="flex flex-col justify-between sticky top-[8rem] h-[80vh]"
         // style={{ borderRight: "1px solid rgb(128, 128, 128, .2)" }}
       >
         <header>
@@ -71,7 +71,7 @@ export default function Home() {
             </a>
           </nav>
         </header>
-        <div className=" flex gap-2 text-3xl items-center ">
+        <div className="flex gap-2 text-3xl items-center ">
           <a
             href="https://www.linkedin.com/in/jing-chun-chiu-4316b420b/"
             target="_blank"
@@ -146,20 +146,30 @@ export default function Home() {
       </div>
 
       <main className="pt-20">
-        <section id="ABOUT" className="scroll-mt-24">
+        <section id="ABOUT" className="scroll-mt-32">
           <p className="mb-2">
             Rewind to 2020, amidst the chaotic pandemic, I made a pivotal
-            decision to pursue a career in
-            <span className="text-sky-400"> web development </span>. Until this
-            day, the creative process of visualizing infomation, data or ideas
-            into something meaningful or concrete still facsinates me.
+            decision to pursue a career in software engineering. Until this day,
+            the creative process of visualizing infomation, data or ideas into
+            something meaningful or concrete still facsinates me.
           </p>
 
           <p className="mb-2">
             Throughout the course of my career, I have contributed to many
             applications—from content management systems, landing pages for
-            e-commerce sites to interfaces for kiosk machines using technologies
-            like<span className="text-cyan-400"> ReactJS </span>,
+            <a
+              className="text-neutral-100"
+              href="https://24h.pchome.com.tw/"
+              target="_blank"
+              rel="noreferrer"
+              title="PChome 24H"
+            >
+              &nbsp;e-commerce site&nbsp;
+            </a>
+            to interfaces for
+            <span className="text-neutral-100"> kiosk machines </span>using
+            technologies like
+            <span className="text-cyan-400"> ReactJS </span>,
             <span className="text-emerald-400"> NextJS</span>,
             <span className="text-violet-400"> Typescript</span>,
             <span className="text-fuchsia-400"> TailwinsCSS </span>and
@@ -177,10 +187,66 @@ export default function Home() {
           </p>
         </section>
 
-        <section className="h-[90vh] mt-24 scroll-mt-24" id="EXPERIENCE">
-          EXPERIENCE
+        <section
+          className="h-[90vh] mt-24 scroll-mt-32 relative"
+          id="EXPERIENCE"
+        >
+          <div className="border-l-2 border-neutral-600 pt-2 relative">
+            {/* Present Position */}
+            <div className="flex items-center ml-8 mb-10 relative">
+              <div className="absolute py-1 bg-[#0b132b] top-0 left-[-60px] text-[12px] w-[55px] break-words">
+                PRESENT
+              </div>
+              <div className="ml-4">
+                <h3 className="text-xl font-semibold">
+                  Senior Software Engineer @XXX Technology
+                </h3>
+                <div className="mt-4 space-y-3">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sint qui laborum adipisci eos ex itaque assumenda.
+                    Assumenda, ipsa blanditiis?
+                  </p>
+                  <p>
+                    Assumenda quod odio eos perspiciatis iure error laboriosam
+                    corporis veritatis optio.
+                  </p>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sint qui laborum adipisci eos ex itaque assumenda.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center ml-8 mb-10 relative">
+              <div className="absolute py-1 bg-[#0b132b] top-0 left-[-60px] text-[12px] w-[55px] break-words">
+                PRESENT
+              </div>
+              <div className="ml-4">
+                <h3 className="text-xl font-semibold">
+                  Senior Software Engineer @XXX Technology
+                </h3>
+                <div className="mt-4 space-y-3">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sint qui laborum adipisci eos ex itaque assumenda.
+                    Assumenda, ipsa blanditiis?
+                  </p>
+                  <p>
+                    Assumenda quod odio eos perspiciatis iure error laboriosam
+                    corporis veritatis optio.
+                  </p>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Sint qui laborum adipisci eos ex itaque assumenda.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
-        <section className="h-[90vh] mt-24 scroll-mt-24" id="PROJECTS">
+
+        <section className="h-[90vh] mt-24 scroll-mt-32" id="PROJECTS">
           PROJECTS
         </section>
       </main>
